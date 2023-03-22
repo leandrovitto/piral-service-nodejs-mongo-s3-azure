1721bba449f09ce04b3a11e2cf55d8263449cd24
Leandro <vitto.leandro@gmail.com>
2023-03-22 15:11:20 +0100

    Add Env Configuration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 /* eslint-disable no-console */
 // eslint-disable-next-line no-console
 import express from 'express';
+import dotenv from 'dotenv';
+
+//-----NODE ENV------
+const PRODUCTION = 'production';
+
+if (process.env.NODE_ENV !== PRODUCTION) {
+  dotenv.config();
+}
+console.log('NODE_ENV:', process.env.NODE_ENV);
+//-----NODE ENV------
+
 const app = express();
-const port = 3000;
+const port = process.env.NODE_PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
